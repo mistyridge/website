dda181a3ba64d7e838d6373f3641303d7a3b07c8
aaronjtonner <tonneraaron@gmail.com>
2022-07-08 16:54:22 -0600

    bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -78,6 +78,50 @@ export default function Bio() {
             * Honey collected from the Misty Ridge Retreat B&B honey bee farm!
           </p>
         </div>
+        <div>
+          <p>
+            The name Misty Ridge dates back to 1992 when we purchased our second
+            acreage as a married couple. The name never left our dreams. Fast
+            forward to 2017. Our urge to leave the city and get back to a
+            peaceful acreage life became our driving force to start searching
+            for a little piece of paradise. After an extensive search, we
+            decided to pause our search for six months. Well, the universe had
+            different plans. One morning we stumbled upon a listing that met our
+            dreams. How could we say no! <br />
+            <br /> We are Allen and Kathryn Kessler. Since 1992 we talked about
+            owning and hosting a traditional bed and breakfast. <br />
+            <br /> Welcome to our little piece of paradise, Misty Ridge Retreat
+            B&B. MRRBB boasts endless peace, tranquility, beauty and connection
+            to nature. The key features of our property offer kilometres of
+            groomed walking trails that can be enjoyed year round. Kids of all
+            ages love the sports field that offers a place to play lawn games or
+            for the happy couple a wonderful place for their reception and
+            dance. Turtle Hill is the perfect place for a summer wedding
+            ceremony under the ivy trailed arch. It is also the perfect place to
+            watch the fireworks during the Calgary Stampede without the crowds.
+            Or star gazing in “Dark Sky County” of Foothills. The gentle rolling
+            slopes are great for summertime slip ’n slide fun or wintertime
+            tobogganing. Throughout the summer the yard is sprinkled with
+            wildflowers and offers the foraging minded a delectable delight. Do
+            you love berries?! Well, we have berries, saskatoon, raspberries and
+            gooseberries. <br />
+            <br /> The trails will take you through the Enchanted Forest where
+            you will find some enchanted creatures, including monkeys! Take a
+            bucket with you so that you can enjoy picking some of those berries.{" "}
+            <br />
+            <br /> The property is uniquely shaped in a panhandle which provides
+            the ultimate in privacy and peace. One last comment, the previous
+            owners blessed this space when they created this piece of paradise.
+            As the foundation was poured soapstone bricks were carved with words
+            and placed around the perimeter of the home. Each word was literally
+            cemented into the foundation. Words like; PEACE (as you walk into
+            the front door), FAMILY (as you sit at our hearth), JOY (in the
+            centre of the home), WEALTH, LOVE, WISDOM, SUCCESS, LEARNING,
+            CREATIVITY, and ADVENTURE. It is our sincere hope that your stay at
+            our little piece of paradise is peaceful, rejuvenating and that you
+            find a way to reconnect with nature. Allen & Kathryn
+          </p>
+        </div>
       </Container>
     </Section>
   )

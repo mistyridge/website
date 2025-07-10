848aa82581ace40fe1186da926c154281a8bf65c
Windsor Fleet <et725560@dal.ca>
2025-07-10 20:06:58 -0300

    Removed Marie from the Pacha Team page as per request.

--- src/components/pachaTeam.js
+++ src/components/pachaTeam.js
@@ -17,7 +17,7 @@ export default function Team() {
             licensed holistic practicioners
           </p>
           <h2 className="title--bold">
-            meet our registered massage therapists
+            meet our registered massage therapist
           </h2>
         </div>
         <div className="spacing-lg">
@@ -54,47 +54,6 @@ export default function Team() {
                 clients walk out of my office with a dreamy, sleepy look. That’s
                 when I know that I have taken them from fight & flight to rest &
                 digest, and their body is beginning to heal.
-              </MainP>
-            </div>
-          </Flex>
-          <Flex>
-            <StaticImage
-              src="../images/pacha-marie-calgary-rmt.jpg"
-              alt="MARIE BÉLANGER calgary rmt"
-              className="stretch"
-            />
-            <div className="spacing">
-              <h3 className="body--large upper bold">marie Bélanger </h3>
-              <MainP>
-                My name is Marie, and I’m a Licensed Holistic Practitioner a
-                2200hr Registered Massage Therapist, a Mom, life partner,
-                sister, friend, health and wellness facilitator, yoga teacher,
-                retreat host, flight attendant and so much more. My journey in
-                the Natural and Holistic practices started in 2014 after a
-                heartbreaking and eye opening diagnosis of PTSD. Years of
-                constant stress, suppressed and overlooked trauma took its toll,
-                not only on my mind but also my body. Exhausting all options in
-                traditional Western medicine I turned to a Holistic approach.
-                Through my own healing I have been humbled and I know it is my
-                purpose to share the wisdom I’ve gained through my experiences
-                and provide support for those facing adversity. My passion is to
-                work WITH you and offer a variety of modalities for your healing
-                and wellness. My offerings included; Massage Therapy (ACMT), The
-                Healing Touch (Verna Besselink), BodyTalk (Allison Bachmeire),
-                Usui Reiki Master, RainDrop Therapy (Joni Brestler), Sound
-                Healing (Life changing energy), Akashic record readings (Linda
-                Howe), Wellness retreats, Sacred Cacao Ceremonies (Embue Cacao,
-                “Into the Heart”), and intuitive guidance. I have also completed
-                a 200hr Embodiment Yoga™ Teacher training with Mandorla Yoga
-                Institute and am honoured to guide you in your personal practice
-                and encourage you to come home to yourself experiencing the
-                richness of living embodied. It is with gratitude that I empower
-                you to trust your intuition and natural abilities for your
-                healing, and wellness. I thank you for your trust in me to
-                honour you, and for allowing yourself to embark on this journey.
-                I believe in you, and YOU ARE WORTH IT!! ~I will meet you right
-                where you are. I offer you my hand, it is up to you to take the
-                first step.~ MB
               </MainP>
             </div>
           </Flex>

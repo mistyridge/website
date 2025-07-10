7064d991023c084938e658bf2bdf22f3b56f5b5a
Windsor Fleet <et725560@dal.ca>
2025-07-10 18:52:35 -0300

    Added css and wrapped pacha-mamma page in a div with class pacha-wrapper to change colour scheme.

--- src/pages/pacha-mamma.js
+++ src/pages/pacha-mamma.js
@@ -9,20 +9,23 @@ import Team from "../components/pachaTeam"
 import Clinic from "../components/pachaClinicInfoGrid"
 import FeaturedFaq from "../components/pachaFaq"
 import Calander from "../components/calander"
+import "../styles/pachaMamma.css"
 
 export default function PachaMammaPage() {
   return (
-    <Layout>
-      <PachaTopBar />
-      <Hero />
-      <Reviews />
-      <Treatments />
-      <Why />
-      <Team />
-      <Clinic />
-      <Calander />
-      <FeaturedFaq />
-      <PachaTopBar />
-    </Layout>
+    <div className="pacha-wrapper">
+      <Layout>
+        <PachaTopBar />
+        <Hero />
+        <Reviews />
+        <Treatments />
+        <Why />
+        <Team />
+        <Clinic />
+        <Calander />
+        <FeaturedFaq />
+        <PachaTopBar />
+      </Layout>
+    </div>
   )
 }
